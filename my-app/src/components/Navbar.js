5f0365b576743193b76dd587d734be40e5856b8a
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-11 19:01:30 -0500

    added some links to the navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const styles = {
   card: {
     margin: 20,
-    background: '#e8eaf6',
+    background: "#e8eaf6",
   },
   heading: {
-    background: '#9a74db',
+    background: "#9a74db",
     minHeight: 50,
     lineHeight: 3.5,
-    fontSize: '1.2rem',
-    color: 'white',
-    padding: '0 20px',
-    display: 'inline',
+    fontSize: "1.2rem",
+    color: "white",
+    padding: "0 20px",
+    display: "inline",
   },
 };
 
-function Navbar() {
+function Navbar({ currentPage, handlePageChange }) {
   return (
     <div style={styles.card}>
-      <div style={styles.heading} Link to="/">About Me</div>
-      <div style={styles.heading} Link to="/portfolio">Portfolio</div>
-      <div style={styles.heading} Link to="/contact">Contact</div>
-      <div style={styles.heading} Link to="/resume">Resume</div>
+      <div style={styles.heading}>
+        <a href="#about" onClick={() => handlePageChange("About")}>
+          About Me
+        </a>
+      </div>
+      <div style={styles.heading}>
+        <a href="#portfolio" onClick={() => handlePageChange("Portfolio")}>Portfolio</a>
+      </div>
+      <div style={styles.heading}>
+        <a href="#contact" onClick={() => handlePageChange("Contact")}>Contact</a>
+      </div>
+      <div style={styles.heading}>
+        <a href="#resume">Resume</a>
+      </div>
     </div>
   );
 }
