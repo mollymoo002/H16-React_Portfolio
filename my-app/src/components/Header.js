191986a027ba0fda93354b3a6ee5fcd572069d7d
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-14 12:47:55 -0500

    added the render page function and the useState from React

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,18 +1,36 @@
-import React from 'react';
-// Here we are importing a CSS file as a dependency
+import React, { useState } from 'react';
 import '../styles/Header.css';
 import Navbar from './Navbar';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
 
-function Header() {
+export default function Header() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if(currentPage === 'About') {
+      return <About />
+    }
+    if(currentPage === 'Projects') {
+      return <Projects />
+    }
+    if(currentPage === 'Contact') {
+      return <Contact />
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <header className="header">
-      <h1>Molly</h1>
-      <Navbar />
-    </header>
+    <div>
+      <header className="header">
+        <h1>Molly</h1>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      </header>
+      <div>
+        {renderPage()}
+      </div>
+    </div>
   );
 }
-
-export default Header;
