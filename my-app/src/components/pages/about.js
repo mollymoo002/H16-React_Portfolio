eb3bd231d58692089aa49b30a2c9d53c25ad517e
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-14 13:23:10 -0500

    created the about page and added my resume in a pdf

--- my-app/src/components/pages/about.js
+++ my-app/src/components/pages/about.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function About() {
     return (
-
+        <div>
+            <h1>About Me</h1>
+            <p>I am 29 years old studying to become a web developer. I live in Pennsylvania with my husband, Dennis</p>
+            <p>Skills: JavaScript, CSS, HTML, JQuery, React, Web APIs, Express.js, Node.js, SQL, MongoDB, Bootstrap</p>
+            <a href="./resources/Resume - Bootcamp.pdf">Resume</a>
+        </div>
     );
 }
