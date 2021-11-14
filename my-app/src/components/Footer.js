5ddbe3f19cb12a9bc886726c860e5e9fe20e8d20
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-14 14:25:41 -0500

    added styling to the footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -6,22 +6,27 @@ const styles = {
         left: 0,
         bottom: 0,
         width: '100%',
-        backgroundColor: 'red',
+        backgroundColor: '#219ade',
         color: 'white',
         textAlign: 'center',
+        padding: 30,
       },
       feet: {
         fontSize: '1.2rem',
         display: 'inline',
         padding: 30,
+      },
+      feetLinks: {
+        color: "white",
+        textDecoration: "none",
       }
 };
 
 function Footer() {
   return (
     <div style={styles.footer}>
-      <div style={styles.feet}><a href="https://mollymoo002.github.io/H2-Portfolio/" target="_blank">Portfolio</a></div>
-      <div style={styles.feet}><a href="https://github.com/mollymoo002" target="_blank">Github</a></div>
+      <div style={styles.feet} ><a style={styles.feetLinks} href="https://mollymoo002.github.io/H2-Portfolio/" target="_blank">Portfolio</a></div>
+      <div style={styles.feet}><a style={styles.feetLinks} href="https://github.com/mollymoo002" target="_blank">Github</a></div>
     </div>
   );
 }
--- my-app/src/components/pages/about.js
+++ my-app/src/components/pages/about.js
@@ -4,6 +4,9 @@ import resume from '../resources/Resume.pdf';
 const styles = {
     titles: {
         textAlign: "center",
+    },
+    aboutDesc: {
+        textAlign: "center",
     }
 }
 
@@ -11,8 +14,8 @@ export default function About() {
     return (
         <div>
             <h1 style={styles.titles}>About Me</h1>
-            <p>I am 29 years old studying to become a web developer. I live in Pennsylvania with my husband, Dennis</p>
-            <p>Skills: JavaScript, CSS, HTML, JQuery, React, Web APIs, Express.js, Node.js, SQL, MongoDB, Bootstrap</p>
+            <p style={styles.aboutDesc}>I am 29 years old studying to become a web developer. I live in Pennsylvania with my husband, Dennis</p>
+            <p style={styles.aboutDesc}>Skills: JavaScript, CSS, HTML, JQuery, React, Web APIs, Express.js, Node.js, SQL, MongoDB, Bootstrap</p>
             <a href={resume} target="_blank">Resume</a>
         </div>
     );
